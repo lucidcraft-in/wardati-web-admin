e01176c8dc849796a891509a1a7825e7fac940fb
SALMAN-FRZ <farisareekkad796@gmail.com>
2022-10-12 13:00:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+import NavBar from './components/layout/NavBar';
+import SideBar from './components/layout/SideBar';
+import DashBoard from './components/dashBoard/DashBoard';
+
+function App() {
+  return (
+    <div className="container-scroller">
+      <NavBar />
+      <div className="container-fluid page-body-wrapper">
+        <SideBar />
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <DashBoard/>
+                  </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
