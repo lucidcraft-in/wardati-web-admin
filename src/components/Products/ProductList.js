a79328ab82e50ae6eed1ff0b5ced4fdd937aba77
mushthaque-mtc <mushthaquemtc@gmail.com>
2022-10-13 15:12:35 +0530

    product list

--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -5,7 +5,73 @@ export default function ProductList() {
     <div class="main-panel">
     <div class="content-wrapper">
         <div class="row">
-          sddsh
+          <div class="col-lg-12 grid-margin stretch-card">
+           <div class="card">
+              <div class="card-body">
+                   <h4 class="card-title">Products</h4>
+                
+                <div class="table-responsive">
+                  <table class="table table-striped">
+                  <thead>
+                        <tr>
+                          <th>
+                            User
+                          </th>
+                          <th>
+                            First name
+                          </th>
+                          <th>
+                            Progress
+                          </th>
+                          <th>
+                            Amount
+                          </th>
+                          <th>
+                            Deadline
+                          </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                          <td  class="py-1">
+                           ddf
+                          </td>
+                          <td>
+                            Herman Beck
+                          </td>
+                          <td>
+                           jjhhj
+                          </td>
+                          <td>
+                            $ 77.99
+                          </td>
+                          <td>
+                            May 15, 2015
+                          </td>
+                      </tr>
+                      <tr>
+                          <td class="py-1">
+                           rerer
+                          </td>
+                          <td>
+                            Messsy Adam
+                          </td>
+                          <td>
+                           rere
+                          </td>
+                          <td>
+                            $245.30
+                          </td>
+                          <td>
+                            July 1, 2015
+                          </td>
+                        </tr>
+                    </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
       </div>
       </div>
