f9728c8ec1e13d26dab2027c8a3cef5f8b6b05c7
Athulsam010 <athulofficial010@gmail.com>
2022-10-13 14:25:01 +0530

    route

---
+++ src/components/SubCategory/CreateSubCategory.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CreateSubCategory() {
+  return (
+    <div>CreateSubCategory</div>
+  )
+}
---
+++ src/components/SubCategory/SubCategoryList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SubCategoryList() {
+  return (
+    <div>SubCategoryList</div>
+  )
+}
