674dc8eab890841397fd4a9575856b108efc1bbb
Athulsam010 <athulofficial010@gmail.com>
2022-10-13 15:30:08 +0530

    stock-user-list

--- src/components/Stock/StockList.js
+++ src/components/Stock/StockList.js
@@ -2,6 +2,67 @@ import React from 'react'
 
 export default function StockList() {
   return (
-    <div>StockList</div>
+    <div className='main-panel'>
+        <div class="content-wrapper">
+        <div class="row">
+          <div class="col-lg-12 grid-margin stretch-card">
+           <div class="card">
+              <div class="card-body">
+                   <h4 class="card-title">Users</h4>
+                
+                <div class="table-responsive">
+                  <table class="table table-striped">
+                  <thead>
+                        <tr>
+                          <th>
+                           Product Name
+                          </th>
+                          <th>
+                             Size
+                          </th>
+                          <th>
+                            Color
+                          </th>
+                          <th>
+                            Count
+                          </th>
+                          
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                          <td  class="py-1">
+                           Athul
+                          </td>
+                          <td>
+                            athulsample@gmail.com
+                          </td>
+                          <td>
+                           Sam
+                          </td>
+                          
+                      </tr>
+                      <tr>
+                          <td  class="py-1">
+                           jon
+                          </td>
+                          <td>
+                            jontest@gmail.com
+                          </td>
+                          <td>
+                           Stalin
+                          </td>
+                          
+                      </tr>
+                     
+                    </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -3,11 +3,64 @@ import React from 'react'
 
 export default function UserList() {
   return (
-    <div>
-       
-        
-        <div>UserList</div>
-        
+    <div className="main-panel">
+        <div class="content-wrapper">
+        <div class="row">
+          <div class="col-lg-12 grid-margin stretch-card">
+           <div class="card">
+              <div class="card-body">
+                   <h4 class="card-title">Users</h4>
+                
+                <div class="table-responsive">
+                  <table class="table table-striped">
+                  <thead>
+                        <tr>
+                          <th>
+                            Name
+                          </th>
+                          <th>
+                             Email
+                          </th>
+                          <th>
+                            Admin
+                          </th>
+                          
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                          <td  class="py-1">
+                           Athul
+                          </td>
+                          <td>
+                            athulsample@gmail.com
+                          </td>
+                          <td>
+                           Sam
+                          </td>
+                          
+                      </tr>
+                      <tr>
+                          <td  class="py-1">
+                           jon
+                          </td>
+                          <td>
+                            jontest@gmail.com
+                          </td>
+                          <td>
+                           Stalin
+                          </td>
+                          
+                      </tr>
+                     
+                    </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
     </div>
     
   )
